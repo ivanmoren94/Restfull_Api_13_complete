2b7ad493d14be6464191593553e33f96003098a7
ivanmoren94 <ivanmoren94@gmail.com>
2023-12-13 08:33:55 +0100

    add files

---
+++ app.js
@@ -0,0 +1,40 @@
+// Importamos el modulo de express
+const express = require("express");
+// Importamos mongo
+const mongoose = require("mongoose");
+// Declaramos el puerto donde se levantará el servidor
+const PORT = 3000;
+const userRouter = require("./router/userRoutes");
+
+//Así inicializamos express y podemos acceder a todas las funcionalidades que nos proporciona
+const app = express();
+
+//Analizamos los archivos JSON
+app.use(express.json());
+
+// Esto nos permite obtener la información de configuración de ".env"
+require("dotenv").config();
+
+const url_mongo = process.env.DATABASE_URL_DEV;
+
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log(`Error al conectar con mongo ${error}`);
+});
+
+db.on("connected", () => {
+  console.log(`Succecss connect`);
+});
+
+db.on("disconected", () => {
+  console.log(`Mongo is disconected`);
+});
+
+app.use("/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
---
+++ controllers/usersControllers.js
@@ -0,0 +1,71 @@
+const userModel = require("../Models/userModels");
+
+let USERS = [
+  { id: 0, name: "Usuario 1", email: "usuario1@example.com" },
+  { id: 1, name: "Usuario 2", email: "usuario2@example.com" },
+  { id: 2, name: "Usuario 3", email: "usuario3@example.com" },
+];
+
+const getUsers = async (req, res) => {
+  const data = await userModel.find();
+  res.status(200).json({ status: "succeeded", data, error: null });
+};
+
+const getUserById = async (req, res) => {
+  const userId = req.params.id;
+  const user = await userModel.findById(userId);
+  res.send(user);
+};
+
+// //Metodo patch para obtener un unico usuario
+const patchById = (req, res) => {
+  //Convertimos el id de la URL en un entero
+  const userId = parseInt(req.params.id);
+  const { name, email } = req.body;
+  //Buscamos en el array por el valor que queremos buscar, en este caso por id
+  const user = USERS.find((user) => user.id === userId);
+
+  if (!user) {
+    res.send("El usuario no existe");
+  }
+
+  if (name) {
+    user.name = req.body.name;
+  }
+
+  if (email) {
+    user.email = req.body.email;
+  }
+
+  res.send(user);
+};
+
+const addUser = (req, res) => {
+  const { name, email } = req.body;
+  const newIndex = USERS.length + 1;
+
+  const newUser = {
+    id: newIndex,
+    name: name,
+    email: email,
+  };
+
+  USERS.push(newUser);
+
+  res.send(newUser);
+};
+
+const deleteUser = (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  const filteredUsers = USERS.filter((user) => user.id !== userId);
+
+  if (filteredUsers.length === USERS.length) {
+    res.send("El usuario no existe");
+  } else {
+    USERS = filteredUsers;
+    res.send(filteredUsers);
+  }
+};
+
+module.exports = { getUsers, getUserById, patchById, addUser, deleteUser };
