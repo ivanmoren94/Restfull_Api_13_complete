faa3c1a4fe4fb85ae4028fc142a28562723cbd21
ivanmoren94 <ivanmoren94@gmail.com>
2023-12-14 08:19:24 +0100

    fix

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -72,19 +72,42 @@ const getProductById = async (req, res) => {
 const updateProduct = async (req, res) => {
   try {
     const productId = req.params.id;
-    const product = await Product.findByIdAndUpdate(productId, req.body, { new: true });
-    if (product) {
-      res.status(200).json({
-        status: "Success",
-        message: "Producto actualizado exitosamente",
-        data: product,
-      });
-    } else {
-      res.status(404).json({
+    const product = await Product.findById(productId);
+    
+    if (!product) {
+      return res.status(404).json({
         status: "Error",
         message: "Producto no encontrado",
       });
     }
+
+    // Actualizar propiedades del producto
+    if (req.body.name) {
+      product.name = req.body.name;
+    }
+    if (req.body.price) {
+      product.price = req.body.price;
+    }
+    if (req.body.description) {
+      product.description = req.body.description;
+    }
+    if (req.body.sizes) {
+      product.sizes = req.body.sizes;
+    }
+    if (req.body.colors) {
+      product.colors = req.body.colors;
+    }
+    if (req.body.brand) {
+      product.brand = req.body.brand;
+    }
+
+    await product.save();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Producto actualizado exitosamente",
+      data: product,
+    });
   } catch (error) {
     res.status(400).json({
       status: "Error",
@@ -93,6 +116,7 @@ const updateProduct = async (req, res) => {
     });
   }
 };
+
 
 const deleteProduct = async (req, res) => {
   try {
